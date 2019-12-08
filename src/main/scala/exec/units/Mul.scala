e3ec8516cf2e151b2b07e3f261d2c39dc1c1c104
Liu Xiaoyi <circuitcoder0@gmail.com>
2019-12-08 08:34:19 +0800

    Extra pipeline stage in Mul to optimize timing

--- src/main/scala/exec/units/Mul.scala
+++ src/main/scala/exec/units/Mul.scala
@@ -7,21 +7,22 @@ import exec._
 import _root_.core.CoreDef
 
 class MulExt(implicit val coredef: CoreDef) extends Bundle {
-  val acc = UInt((coredef.XLEN).W)
+  val x1 = SInt((coredef.XLEN).W)
+  val x2 = SInt((coredef.XLEN).W)
 }
 
-class Mul(override implicit val coredef: CoreDef) extends ExecUnit(1, new MulExt) {
+class Mul(override implicit val coredef: CoreDef) extends ExecUnit(2, new MulExt) {
   assert(coredef.XLEN == 64)
 
   override def map(stage: Int, pipe: PipeInstr, _ext: Option[MulExt]): (MulExt, Bool) = {
-    if(stage == 0) {
-      // printf(p"[MUL  0]: COMP ${Hexadecimal(pipe.rs1val)} * ${Hexadecimal(pipe.rs2val)}\n")
-      val ext = Wire(new MulExt)
+    val isDWord = (
+      pipe.instr.instr.op === Decoder.Op("OP-IMM").ident
+      || pipe.instr.instr.op === Decoder.Op("OP").ident
+    )
 
-      val isDWord = (
-        pipe.instr.instr.op === Decoder.Op("OP-IMM").ident
-        || pipe.instr.instr.op === Decoder.Op("OP").ident
-      )
+    if(stage == 0) {
+      // Pipelining requests
+      val ext = Wire(new MulExt)
 
       val (op1, op2) = (Wire(SInt(coredef.XLEN.W)), Wire(SInt(coredef.XLEN.W)))
 
@@ -33,35 +34,41 @@ class Mul(override implicit val coredef: CoreDef) extends ExecUnit(1, new MulExt
         op2 := pipe.rs2val(31, 0).asSInt
       }
 
+      ext.x1 := op1
+      ext.x2 := op2
+      return (ext, false.B)
+    } else if(stage == 1) {
+      // printf(p"[MUL  0]: COMP ${Hexadecimal(pipe.rs1val)} * ${Hexadecimal(pipe.rs2val)}\n")
+      val prev = _ext.get
+      val ext = Wire(new MulExt)
+      ext.x2 := DontCare
+
       when(!isDWord) {
         // Can only be MULW
-        val extended = Wire(SInt(64.W))
-        extended := (op1 * op2)(31, 0).asSInt()
-        ext.acc := extended.asUInt()
+        ext.x1 := (prev.x1 * prev.x2)(31, 0).asSInt()
       }.otherwise{
-        ext.acc := DontCare
+        ext.x1 := DontCare
         switch(pipe.instr.instr.funct3) {
           is(Decoder.MULDIV_FUNC("MUL")) {
-            ext.acc := (op1 * op2).asUInt()(63, 0)
+            ext.x1 := (prev.x1 * prev.x2)(63, 0).asSInt
           }
 
           is(Decoder.MULDIV_FUNC("MULH")) {
-            ext.acc := (op1 * op2).asUInt()(127, 64)
+            ext.x1 := (prev.x1 * prev.x2)(127, 64).asSInt
           }
 
           is(Decoder.MULDIV_FUNC("MULHU")) {
-            ext.acc := (op1.asUInt * op2.asUInt).asUInt()(127, 64)
+            ext.x1 := (prev.x1.asUInt * prev.x2.asUInt)(127, 64).asSInt
           }
 
           is(Decoder.MULDIV_FUNC("MULHSU")) {
-            ext.acc := (op1 * op2.asUInt).asUInt()(127, 64)
+            ext.x1 := (prev.x1 * prev.x2.asUInt)(127, 64).asSInt
           }
         }
       }
 
-      // Never stalls
       (ext, false.B)
-    } else if(stage == 1) {
+    } else if(stage == 2) {
       (_ext.get, false.B)
     } else {
       throw new Error(s"Unexpected stage $stage in Mul module")
@@ -74,7 +81,7 @@ class Mul(override implicit val coredef: CoreDef) extends ExecUnit(1, new MulExt
     info.mem.noop()
 
     // info.regWaddr := pipe.instr.instr.rd
-    info.wb := ext.acc
+    info.wb := ext.x1.asUInt
 
     info
   }
